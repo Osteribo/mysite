e11a352515f840075a70fc4b74166b6a08b82e96
ALO <36780370+Osteribo@users.noreply.github.com>
2020-06-13 10:26:16 -0700

    UPDATE: Modularize views.py and urls.py
    converted "def" to "class" types to modularize views.py.
    
    urls.py updated 'pk' referecences to integrate with modularization.

--- polls/views.py
+++ polls/views.py
@@ -1,30 +1,50 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 
 # Create your views here. will always take in "request" object
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    output = ', '.join([q.question_text for q in latest_question_list])
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     output = ', '.join([q.question_text for q in latest_question_list])
 
-    return render(request, 'polls/index.html', context)
+#     return render(request, 'polls/index.html', context)
 
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+# update with classes instead of def
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """ Return the last five published questions """
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
@@ -37,7 +57,7 @@ def vote(request, question_id):
             'question': question,
             'error_message': "You did not select a choice.",
         })
-    else: 
+    else:
         selected_choice.votes += 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
